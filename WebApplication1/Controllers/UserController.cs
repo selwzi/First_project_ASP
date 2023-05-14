431e3b19a7808f4e90549d2d11cfa4060fb4fe78
selwzi <ivshin.donel@gmail.com>
2023-05-15 00:05:52 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using WebApplication1.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly ILogger<UserController> _logger;
+
+		public UserController(ILogger<UserController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(User user)
+		{
+			user.OnlyDate();
+			string json = JsonConvert.SerializeObject(user);
+			var arr = json.Split(",");
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				System.IO.File.AppendAllText("output.json", arr[i] + "\n");
+			}
+
+			return RedirectToAction("Success");
+		}
+
+		public ActionResult Success()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string BirthDate { get; set; }
+
+        public void OnlyDate()
+        {
+            BirthDate = DateTime.Now.ToString("yyyy-MM-dd");
+		}
+    }
+}
